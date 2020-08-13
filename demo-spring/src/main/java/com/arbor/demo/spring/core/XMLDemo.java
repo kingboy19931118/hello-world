7d11e83d99deee9a9d7bc340d9125d105009dfb8
Joem <wangqiaomu@aikucun.com>
2020-08-13 09:24:05 +0800

    beanDefinition

--- demo-spring/src/main/java/com/arbor/demo/spring/core/XMLDemo.java
+++ demo-spring/src/main/java/com/arbor/demo/spring/core/XMLDemo.java
@@ -1,5 +1,9 @@
 package com.arbor.demo.spring.core;
 
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.DefaultBeanNameGenerator;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Date;
@@ -17,6 +21,12 @@ public class XMLDemo {
         System.out.println(new Date().toString());
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:/dev/sping-context.xml");
         SimpleBean simpleBean = (SimpleBean) context.getBean("simpleBean");
+
+        DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) context.getBeanFactory();
+        GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+        beanDefinition.setBeanClass(String.class);
+        beanFactory.registerBeanDefinition("testService", beanDefinition);
+
         simpleBean.send();
         context.close();
     }
